ae552410944d1475e879df6d7e89636f7bff6d64
James Perrin <countrydawgg@gmail.com>
2020-08-05 18:10:19 -0700

    Add onVideoSelect event

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
-import config from "../config/config";
+import config from '../config/config';
 import VideoList from './VideoList';
 
 const KEY = `${config.ApiKeyValue}`;
 
 // NOTE: Had to move the params due to an Axios Version Bug & 400 "Required Parameter: part" Error
 class App extends React.Component {
-    state = { videos: [] };
+    state = { videos: [], selectedVideo: null };
 
     onTermSubmit = async (term) => {
         const response = await youtube.get('/search', {
@@ -17,18 +17,25 @@ class App extends React.Component {
                 type: 'video',
                 maxResults: 5,
                 key: KEY,
-                q: term
-            }
+                q: term,
+            },
         });
 
         this.setState({ videos: response.data.items });
+    };
+
+    onVideoSelect = (video) => {
+        console.log('From the app!', video);
     };
 
     render() {
         return (
             <div className="ui container">
                 <SearchBar onFormSubmit={this.onTermSubmit} />
-                <VideoList videos={this.state.videos} />
+                <VideoList
+                onVideoSelect={this.onVideoSelect}
+                videos={this.state.videos}
+                />
             </div>
         );
     }
--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './VideoItem.css';
 
-const VideoItem = ({ video }) => {
+const VideoItem = ({ video, onVideoSelect }) => {
     const { title, thumbnails } = video.snippet;
     return (
-        <div className="item video-item">
-            <img  className="ui image" src={thumbnails.medium.url} />
+        <div onClick={() => onVideoSelect(video)} className="item video-item">
+            <img className="ui image" src={thumbnails.medium.url} alt={title}/>
             <div className="content">
                 <div className="header">{title}</div>
             </div>
