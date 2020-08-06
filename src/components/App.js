38866d1cd4351bd74219b8d1bfb7a7b87a6cf95f
James Perrin <countrydawgg@gmail.com>
2020-08-05 18:53:26 -0700

    Add selection of a default video

--- src/components/App.js
+++ src/components/App.js
@@ -11,6 +11,10 @@ const KEY = `${config.ApiKeyValue}`;
 class App extends React.Component {
     state = { videos: [], selectedVideo: null };
 
+    componentDidMount() {
+        this.onTermSubmit('buildings');
+    }
+
     onTermSubmit = async (term) => {
         const response = await youtube.get('/search', {
             params: {
@@ -22,11 +26,12 @@ class App extends React.Component {
             },
         });
 
-        this.setState({ videos: response.data.items });
+        this.setState({ videos: response.data.items,
+            selectedVideo: response.data.items[0]  });
     };
 
     onVideoSelect = (video) => {
-        console.log('From the app!', video);
+        // console.log('From the app!', video);
         this.setState({ selectedVideo: video });
     };
 
@@ -34,8 +39,17 @@ class App extends React.Component {
         return (
             <div className="ui container">
                 <SearchBar onFormSubmit={this.onTermSubmit} />
-                <VideoDetail video={this.state.selectedVideo} />
-                <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
+
+                <div className="ui grid">
+                    <div className="ui row">
+                        <div className="eleven wide column">
+                            <VideoDetail video={this.state.selectedVideo} />
+                        </div>
+                        <div className="five wide column">
+                            <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
