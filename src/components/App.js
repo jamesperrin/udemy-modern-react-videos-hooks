e9e5e32ef5e42b5a35d0867f32510d6003dc4235
James Perrin <countrydawgg@gmail.com>
2020-08-05 17:32:01 -0700

    Add VideoList component
    1. Refactor components folder.
    2. Add VideoList component.
    3. Add videos as props to VideoList component.

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import SearchBar from './SearchBar/SearchBar';
+import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
 import config from "../config/config";
+import VideoList from './VideoList';
 
 const KEY = `${config.ApiKeyValue}`;
 
 // NOTE: Had to move the params due to an Axios Version Bug & 400 "Required Parameter: part" Error
 class App extends React.Component {
-    onTermSubmit = (term) => {
-        youtube.get('/search', {
+    state = { videos: [] };
+
+    onTermSubmit = async (term) => {
+        const response = await youtube.get('/search', {
             params: {
                 part: 'snippet',
                 type: 'video',
@@ -17,12 +20,15 @@ class App extends React.Component {
                 q: term
             }
         });
+
+        this.setState({ videos: response.data.items });
     };
 
     render() {
         return (
             <div className="ui container">
                 <SearchBar onFormSubmit={this.onTermSubmit} />
+                <VideoList videos={this.state.videos} />
             </div>
         );
     }
---
+++ src/components/VideoList.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const VideoList = props => {
+    // props.videos
+    return <div>I have {props.videos.length} videos</div>;
+};
+
+export default VideoList;
